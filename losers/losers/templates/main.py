5c4ae1467f4ceffa95d167b596292c16301f7503
Pablo Buenaposada <pablo.buenaposada@elementsinteractive.es>
2019-10-18 13:32:24 +0200

    time logic

--- losers/losers/templates/main.py
+++ losers/losers/templates/main.py
@@ -1,26 +1,46 @@
 import threading
 import time
-from random import randint
+
+INIT_TIME = 10
+INIT_RESEARCH = 100
+INIT_HAPPINESS = 100
+RESTART_WAITING_TIME = 2
 
 
 class Main:
-    happiness = 0
-    research = 0
-    time = 666
+    happiness = INIT_HAPPINESS
+    research = INIT_RESEARCH
+    time = INIT_TIME
     last_time = 0
+    result = ""
+    restart_time = 0
 
     def __init__(self):
         t = threading.Thread(target=self.main)
         t.start()
 
-    def minute_passed(self):
+    def second_passed(self):
         if time.time() - self.last_time >= 2:
             self.last_time = time.time()
             return True
 
     def main(self):
         while True:
-            if self.minute_passed():
-                self.time = self.time - 1
+            if self.time > 0:
+                if self.second_passed():
+                    self.time = self.time - 1
 
-            self.research = randint(1, 100)
+                    self.research = self.research - 1
+                    self.happiness = self.happiness - 1
+
+            else:
+                self.result = "You lose"
+                if self.second_passed():
+                    self.restart_time = self.restart_time + 1
+                if self.restart_time > RESTART_WAITING_TIME:
+                    self.time = INIT_TIME
+                    self.restart_time = 0
+                    self.result = ""
+
+                    self.research = INIT_RESEARCH
+                    self.happiness = INIT_HAPPINESS
--- losers/losers/views.py
+++ losers/losers/views.py
@@ -2,4 +2,7 @@ from django.shortcuts import render
 
 
 def index(request, main):
-    return render(request, 'index.html', {'research': main.research, 'time': main.time})
+    return render(request, 'index.html', {'research': main.research,
+                                          'time': main.time,
+                                          'result': main.result,
+                                          'happiness': main.happiness})

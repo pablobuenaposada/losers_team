ad2a09453cc2fc89f9a56b5f788e7ba9c610277f
Marc Benet <marc.benet@elementsinteractive.es>
2019-10-18 14:51:47 +0200

    Adds random events

--- losers/losers/templates/main.py
+++ losers/losers/templates/main.py
@@ -1,11 +1,14 @@
 import threading
 import time
+import random
 from losers.wiregame import WireGame
 
 INIT_TIME = 10
 INIT_RESEARCH = 100
 INIT_HAPPINESS = 100
+INIT_PRODUCTION = 100
 RESTART_WAITING_TIME = 2
+RESOURCE_DELTA = 10
 
 
 class Main:
@@ -19,6 +22,7 @@ class Main:
     # time things
     time = INIT_TIME
     last_time = 0
+    five_last_time = 0
     restart_time = 0
 
     too_fast = False
@@ -35,16 +39,34 @@ class Main:
             self.last_time = time.time()
             return True
 
+    def five_second_passed(self):
+        if time.time() - self.five_last_time >= 10:
+            self.five_last_time = time.time()
+            return True
+
+    def get_random_event(self):
+        event_list = [
+            {"result": "Anarchy", "research_delta": 0,  "happiness_delta": 0, "prodcution_delta": 0},
+            {"result": "Our colony’s engineers go on strike and the production slows down", "research_delta": 0,  "happiness_delta": 0, "prodcution_delta": -RESOURCE_DELTA},
+            {"result": "BBQ day increases heavily the consumption, people are unhappy", "research_delta": 0,  "happiness_delta": -RESOURCE_DELTA, "prodcution_delta": 0},
+            {"result": "Our scientists are ill because bad chemicals leaked in the lab", "research_delta": -RESOURCE_DELTA,  "happiness_delta": 0, "prodcution_delta": 0},
+            {"result": "Severe pests spread throughout the colony, we need immediate action", "research_delta": 0,  "happiness_delta": -RESOURCE_DELTA, "prodcution_delta": 0},
+            {"result": "Recent rainfalls improved the agricultural production, people are very happy with the abundance of food", "research_delta": 0,  "happiness_delta": 0, "prodcution_delta": RESOURCE_DELTA},
+            {"result": "Our researchers just discovered a new resource that can be eaten", "research_delta": RESOURCE_DELTA,  "happiness_delta": RESOURCE_DELTA, "prodcution_delta": RESOURCE_DELTA},
+            {"result": "Thanks to a new discovery, our colony uses less energy", "research_delta": -RESOURCE_DELTA,  "happiness_delta": RESOURCE_DELTA, "prodcution_delta": 0},
+        ]
+        return event_list[random.randint(0,7)]
+
     def main(self):
         while True:
             if self.time > 0:
                 if self.second_passed():
                     self.time = self.time - 1
                     self.too_fast = False
-                    
                     self.research = self.research - 1
                     self.happiness = self.happiness - 1
-                    
+                    self.prodcution = self.prodcution - 1
+
                 if not self.too_fast and self.wg.hit_wire():
                     self.research = self.research - 10
                     self.too_fast = True
@@ -52,7 +74,13 @@ class Main:
                 if not self.too_fast and self.wg.hit_end():
                     self.research = self.research + 50
                     self.too_fast = True
-                    
+
+                if self.five_second_passed():
+                    random_result = self.get_random_event()
+                    self.result = random_result["result"]
+                    self.research = self.research + random_result["research_delta"]
+                    self.happiness = self.happiness + random_result["happiness_delta"]
+                    self.prodcution = self.prodcution + random_result["prodcution_delta"]
 
             else:
                 self.result = "You lose"
@@ -65,3 +93,4 @@ class Main:
 
                     self.research = INIT_RESEARCH
                     self.happiness = INIT_HAPPINESS
+                    self.prodcution = INIT_PRODUCTION
